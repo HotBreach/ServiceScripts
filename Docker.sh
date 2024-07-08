302786b77c9d9154cba9bbe24a7162fd2a7c429a
HotBreach <i.rogozhnikov15@yandex.ru>
2024-07-08 14:41:36 +0500

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -12,3 +12,12 @@ echo "Введите имя пользователя"
 read -r user
 sudo usermod -aG docker "$user"
 echo "Выполнено"
+######Установка агента
+sudo docker run -d \
+  -p 9001:9001 \
+  --name portainer_agent \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+  portainer/agent:2.19.5
+echo "Выполнено!"
