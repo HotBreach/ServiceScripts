deee2d63e22ef00cc079df309540ffec60e07d7c
HotBreach <i.rogozhnikov15@yandex.ru>
2024-07-08 14:34:13 +0500

    Create Docker.sh

---
+++ Docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
+echo "Введите имя пользователя"
+# shellcheck disable=SC2034
+read -r user
+sudo usermod -aG docker "$user"
+echo "Выполнено"
